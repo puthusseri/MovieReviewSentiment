ef9504193a611f17b5d3b53873cca8cd7374b2d5
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2021-05-29 21:56:40 +0530

    feat: updated the code for second student

--- server.py
+++ server.py
@@ -3,13 +3,78 @@ from flask import *
 import preditSentimentByMovieName as byName
 import preditSentimentByMovieLink as byLink
 import mainCode as mainCode
-import language_tool_python
-tool = language_tool_python.LanguageTool('en-US')
+# import language_tool_python
+# tool = language_tool_python.LanguageTool('en-US')
 
-
+import mysql.connector
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+def loginPage():
+    return render_template("login.html")
+
+@app.route("/registerUser")
+def registerUser():
+    # define the method for registration process
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="vyshak-zt515",
+    password="password",
+    database="imdb"
+    )
+    mycursor = mydb.cursor()
+    name = request.args.get('name')
+    username = request.args.get('username')
+    password = request.args.get('password')
+    sql = "INSERT INTO USER(NAME, USERNAME, PASSWORD) VALUES (%s, %s,%s)"
+    val = (name, username, password)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    mydb.close()
+    return render_template("login.html" ,messageFromServer="Registration Successful")
+
+@app.route("/loginToUser")
+def loginToUser():
+    # define the method for login process
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="vyshak-zt515",
+    password="password",
+    database="imdb"
+    )
+    mycursor = mydb.cursor()
+    username = request.args.get('username')
+    password = request.args.get('password')
+    sql = "SELECT * FROM USER WHERE USERNAME = '"+username+"' AND PASSWORD = '"+ password +"';"
+
+    mycursor.execute(sql)
+    account = mycursor.fetchall()
+    mydb.close()
+    if len(account) > 0:
+        # the login is valid
+        # check if the login is by admin
+        if account[0][1] == "admin":
+            resp = make_response(render_template('admin.html'))
+            resp.set_cookie('name', 'admin')
+            resp.set_cookie('user_id', str(account[0][0]))
+        else:
+            resp = make_response(render_template('index.html'))
+            resp.set_cookie('name', account[0][1])
+            resp.set_cookie('user_id', str(account[0][0]))
+        
+        return resp
+    else:
+        pass
+    return render_template("login.html",messageFromServer="Login Unsuccessful")
+
+
+
+@app.route('/logoutButton')
+def logoutButton():
+    resp = make_response(render_template('login.html'))
+    resp.set_cookie('name', " ")
+    return resp
+@app.route('/index')
 def indexpage():
     return render_template("index.html")
 @app.route('/home')
@@ -27,6 +92,9 @@ def redirectToSearchMovieByName():
 @app.route('/redirectToSearchMovieByLink')
 def redirectToSearchMovieByLink():
     return render_template("searchMovieByLink.html")
+@app.route('/loadSearchById')
+def redirectToloadSearchById():
+    return render_template("searchByIdTransactions.html")
 
 
 @app.route('/goToEnterMovieReview')
@@ -44,12 +112,12 @@ def analyseReview():
     movie_review = request.args.get("movie_review")
     # print(movie_review)
     # check if the review 
-    matches = tool.check(movie_review)
-    if len(matches) > 0:
+    # matches = tool.check(movie_review)
+    # if len(matches) > 0:
         # the review is not having good grammer
-        print(f"Invalid Grammer. Number of grammer errors are : {len(matches)}")
-        return "Invalid Grammer"
-    print("There are no grammer mistakes in the review text")
+    #     print(f"Invalid Grammer. Number of grammer errors are : {len(matches)}")
+    #     return "Invalid Grammer"
+    # print("There are no grammer mistakes in the review text")
     result = mainCode.mainMethod(movie_review)
     if result == "Positive Review":
         return "1"
@@ -80,5 +148,72 @@ def contactPageDetails():
     return "True"
 
 
+
+@app.route('/storeReviewFeedBackToDB')
+def storeReviewFeedBackToDB():
+    #  Store the content to the database
+    movie_review = request.args.get("movie_review")
+    typeOfContent = request.args.get("typeOfContent")
+    feedback = request.args.get("feedback")
+    user_id = request.args.get("user_id")
+    
+    # define the method for db insert process
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="vyshak-zt515",
+    password="password",
+    database="imdb"
+    )
+    mycursor = mydb.cursor()
+    
+    sql = "INSERT INTO TRANSACTIONS(USER_ID, TYPE_OF_CONTENT, REVIEW, FEEDBACK) VALUES (%s, %s,%s, %s)"
+    val = (user_id, typeOfContent, movie_review, feedback)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    mydb.close()
+    
+    return "True"
+
+@app.route('/searchTranasactionByid')
+def searchTranasactionByid():
+    user_id = request.args.get("user_id")
+    
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="vyshak-zt515",
+    password="password",
+    database="imdb"
+    )
+    mycursor = mydb.cursor()
+    
+    sql = "SELECT * FROM TRANSACTIONS WHERE USER_ID = '"+user_id+"';"
+    mycursor.execute(sql)
+    account = mycursor.fetchall()
+    
+    mydb.close()
+
+    return render_template("tables.html", account = account)
+
+
+@app.route('/searchTranasactions')
+def searchTranasactions():
+    user_id = request.args.get("user_id")
+    
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="vyshak-zt515",
+    password="password",
+    database="imdb"
+    )
+    mycursor = mydb.cursor()
+    
+    sql = "SELECT * FROM TRANSACTIONS;"
+    mycursor.execute(sql)
+    account = mycursor.fetchall()
+    
+    mydb.close()
+
+    return render_template("tables.html", account = account)
+
 if __name__ == "__main__":
-    app.run(threaded=False)
+    app.run(threaded=False, debug = True)
--- static/script.js
+++ static/script.js
@@ -9,7 +9,27 @@ setInterval(() => {
  }
 }, 42);
 
-
+function getCookie(name) {
+    // Add the = sign
+    name = name + '=';
+    // Get the decoded cookie
+    var decodedCookie = decodeURIComponent(document.cookie);
+  
+    // Get all cookies, split on ; sign
+    var cookies = decodedCookie.split(';');
+  
+    // Loop over the cookies
+    for (var i = 0; i < cookies.length; i++) {
+      // Define the single cookie, and remove whitespace
+      var cookie = cookies[i].trim();
+  
+      // If this cookie has the name of what we are searching
+      if (cookie.indexOf(name) == 0) {
+        // Return everything after the cookies name
+        return cookie.substring(name.length, cookie.length);
+      }
+    }
+  }
 
 // function findByName(name){
 //     alert(name);
